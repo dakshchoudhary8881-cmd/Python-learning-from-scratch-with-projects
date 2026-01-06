fd952b1e5a61f0db51e5961f7bb09fee11a77494
Daksh choudhary <dakshchoudhary8881@gmail.com>
2026-01-06 23:24:47 +0530

    fix bug

--- 01_Project_1/Project_1.py
+++ 01_Project_1/Project_1.py
@@ -8,8 +8,8 @@ number1 = float(input("Enter first number: "))
 number2 = float(input("Enter second number: "))
 
 print("Addition:", number1, "+", number2, "=", number1 + number2)
-print("Subtraction:", number1, "-", number2, "=", number1 - number2)
-print("Multiplication:", number1, "*", number2, "=", number1 * number2)
+print("Subtraction:", number1, "-", number2, "=", number1 - number2 )
+print("Multiplication:", number1, "*", number2, "=", number1 * number2 )
 
 if number2 != 0:
     print("Division:", number1, "/", number2, "=", number1 / number2)
